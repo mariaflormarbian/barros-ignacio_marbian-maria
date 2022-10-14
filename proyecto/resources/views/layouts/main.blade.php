e0114d22d0876b3019fa555e2f7962940efe4057
Nacho1714 <102827516+Nacho1714@users.noreply.github.com>
2022-10-13 21:15:45 -0300

    Layaout - Home(incompleto)

--- proyecto/resources/views/layouts/main.blade.php
+++ proyecto/resources/views/layouts/main.blade.php
@@ -1,134 +1,165 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') : : DV Peliculas</title>
+    <title>@yield('title') : : Ieta</title>
 
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    <!-- Iconos -->
+    <link rel="stylesheet" href="{{ url('css/line-awesome.min.css') }}">
+    <!-- Css -->
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 
 </head>
+
 <body>
 
-    <div id="app">
-    
-        <nav class="navbar navbar-expand-lg bg-light">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
 
-            <div class="container-fluid">
+        <div class="container">
 
-                <a class="navbar-brand" href="{{ route('home') }}">DV Películas</a>
+            <img src="{{ url('imgs/logo1.png') }}" alt="">
 
-                <button 
-                    class="navbar-toggler" 
-                    type="button" 
-                    data-bs-toggle="collapse" 
-                    data-bs-target="#navbarNav" 
-                    aria-controls="navbarNav" 
-                    aria-expanded="false" 
-                    aria-label="Mostrar/Ocultar Navegacion">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span><i id="bar" class="las la-bars"></i></span>
+            </button>
 
-                    <span class="navbar-toggler-icon"></span>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                </button>
+                <ul class="navbar-nav ml-auto">
 
-                <div class="collapse navbar-collapse" id="navbarNav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Catálogo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre Nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <i class="las la-search"></i>
+                        <i class="las la-shopping-bag"></i>
+                    </li>
 
-                    <ul class="navbar-nav">
+                </ul>
+                    
+            </div>
+            
+        </div>
 
-                        <li class="nav-item">
-                            
-                            <!-- La funcion url() crea una URL absoluta a la carpeta "public" del proyecto, de manera dinamica. 
-                            
-                            Usamos la funcion route() para indicar el nombre que asociamos en [proyecto\routes\web.php] a la ruta que le corresponde. De esta manera, si queremos cambiar el nombre original de la ruta es mas facil ya que queda asignado por medio de la variable.
-                            -->
+    </nav>
 
-                            <a class="nav-link" href="{{ route('home') }}">Home</a>
-                        </li>
+    <main>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('quienes-somos') }}">Quienes Somos</a>
-                        </li>
+        @yield('main')
 
-                        {{-- El metodo "check()" de Auth retorna true si el usuario esta autenticado --}}
-                        {{-- @if (Auth::check()) --}}
-                        {{-- Podemos tambien usar las directivas de Blade para este fin --}}
-                        @auth
+    </main>
 
-                            {{-- Para defirnir quien tiene acceso a cada pantalla y evitar que el usuario entre cambiando la url utilizamos un middlewar en [proyecto\routes\web.php] --}}
-                            
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.peliculas.index') }}">Administrar Películas</a>
-                            </li>
-                            
-                            <li class="nav-item">
-                                <form action="{{ route('auth.logout') }}" method="POST">
-                                    @csrf
-                                    <button class="btn nav-link" type="submit">Cerrar Sesion</button>
-                                </form>
-                            </li>
-                        
-                        {{-- Si es, en cambio, un 'invitado'... --}}
-                        @else
+    <footer class="mt-5 py-5">
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('auth.login.form') }}">Iniciar Sesion</a>
-                            </li>
+        <div class="row container mx-auto pt-5">
 
-                        @endauth
-                        {{-- @endif --}}
+            <div class="footer-one col-lg-3 col-md-6 col-12">
 
-                    </ul>
+                <img src="{{ url() }}imgs/logo2.png" alt="">
+
+                <p class="pt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus ea alias odio.</p>
+
+            </div>
+
+            <div class="footer-one col-lg-3 col-md-6 col-12 mb-3">
+
+                <h5 class="pb-2">Featured</h5>
+
+                <ul class="text-uppercase list-unstyled">
+
+                    <li><a href="#">men</a></li>
+                    <li><a href="#">women</a></li>
+                    <li><a href="#">boys</a></li>
+                    <li><a href="#">girls</a></li>
+                    <li><a href="#">new arrivals</a></li>
+                    <li><a href="#">shoes</a></li>
+                    <li><a href="#">cloths</a></li>
+
+                </ul>
+
+            </div>
+
+            <div class="footer-one col-lg-3 col-md-6 col-12 mb-3">
+
+                <h5 class="pb-2">Contact Uss</h5>
+
+                <div>
+                    <h6 class="text-uppercase">Address</h6>
+                    <p>123 STREET NAME, CITY, US</p>
+                </div>
+
+                <div>
+                    <h6 class="text-uppercase">Phone</h6>
+                    <p>(123) 456-789</p>
+                </div>
+
+                <div>
+                    <h6 class="text-uppercase">Address</h6>
+                    <p>123 STREET NAME, CITY, US</p>
+                </div>
+
+            </div>
+
+            <div class="footer-one col-lg-3 col-md-6 col-12 mb-3">
+
+                <h5 class="pb-2">Instagram</h5>
+
+                <div class="row">
+
+                    <img class="img-fluid w-25 h-100 m-2" src="{{ url('img/insta/1.jpg') }}" alt="">
+                    <img class="img-fluid w-25 h-100 m-2" src="{{ url('img/insta/2.jpg') }}" alt="">
+                    <img class="img-fluid w-25 h-100 m-2" src="{{ url('img/insta/3.jpg') }}" alt="">
+                    <img class="img-fluid w-25 h-100 m-2" src="{{ url('img/insta/4.jpg') }}" alt="">
+                    <img class="img-fluid w-25 h-100 m-2" src="{{ url('img/insta/5.jpg') }}" alt="">
 
                 </div>
 
             </div>
 
-        </nav>
+        </div>
 
-        <main class="container py-3">
+        <div class="copyright mt-5">
 
-            {{-- Session es la clase para manejo de sesiones de Laravel --}}
-            @if (Session::has('statusMessage'))
+            <div class="row container mx-auto">
 
-                <div class="alert alert-{{ Session::get('statusType') ?? 'info' }} mb-3">{!! Session::get('statusMessage') !!}</div>
-                
-            @endif
+                <div class="col-lg-3 col-md-6 col-12 mb-4">
+                    <img src="img/payment.png" alt="">
+                </div>
+                <div class="col-lg-4 col-md-6 col-12 text-nowrap mb-2">
+                    <p>rymo eCommerce &copy; 2021. All Rights Reserved</p>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <a href="#"><i class="lab la-facebook-f"></i></a>
+                    <a href="#"><i class="lab la-twitter"></i></a>
+                    <a href="#"><i class="lab la-linkedin-in"></i></a>
+                </div>
 
+            </div>
 
-            {{-- La directiva @yield define un espacio que este archivo va a "ceder" a cualquier otra vista que "extienda" o "herede" de este template. --}}
-            @yield('main')
-        </main>
+        </div>
 
-        <footer class="footer">
-            <p>Da Vinci &copy; 2022</p>
-        </footer>
+    </footer>
 
-    </div>
+    <!-- Bootstrap -->
+    <script src="{{ url('js/bootstrap.bundle.min.js') }}"></script>
 
 </body>
+
 </html>
